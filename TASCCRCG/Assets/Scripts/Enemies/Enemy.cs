891129a8de6bf50cd122d1e5a069e8fda948c9e5
Vincent St-Pierre <vstpierre1@gmail.com>
2026-08-03 11:23:37 -0400

    Enemy Class Parent Class + Wrench Class inheritance
    Reworked Wrench class to simplify by inheriting functions for parent class Enemy instead

--- TASCCRCG/Assets/Scripts/Enemies/Enemy.cs
+++ TASCCRCG/Assets/Scripts/Enemies/Enemy.cs
@@ -3,19 +3,91 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
-
-    // Enemy fields
-
 {
-    // Start is called before the first frame update
-    void Start()
+    // Enemy Fields
+    protected GameManager gameManager;
+    private float damageFlashDuration = 0.1f;
+    private SpriteRenderer spriteRenderer;
+    protected bool isFlashing = false;
+    protected int health;
+    protected int scoreValue;
+
+    protected virtual void Awake()
+    {
+        gameManager = GameObject.FindAnyObjectByType<GameManager>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    protected virtual void Start()
+    {
+
+    }
+
+    protected virtual void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    // Behaviour when the enemy is hit
+    protected virtual void TakeDamage()
     {
-        
+        health--;
+
+        if (health <= 0)
+        {
+            OnDestroyed();
+        }
+        else
+        {
+            // avoids flashing multiple time at the same time
+            if (!isFlashing)
+            {
+                StartCoroutine(DamageFlashCoroutine());
+            }
+        }
     }
+
+    // Behaviour when the enemy is destroyed
+    protected virtual void OnDestroyed()
+    {
+        gameManager.playerManager.AddScore(scoreValue);
+
+        // Powerups and other effects when destroyed
+        gameManager.EnemyDown(this.gameObject);
+        GameObject.Destroy(this.gameObject);
+    }
+
+    private IEnumerator DamageFlashCoroutine()
+    {
+        isFlashing = true;
+
+        // Enemy flashes quickly when taking damage.
+        spriteRenderer.enabled = false;
+        yield return new WaitForSeconds(damageFlashDuration);
+        spriteRenderer.enabled = true;
+
+        isFlashing = false;
+
+
+    }
+
+    // Handling collision to avoid OnCollisionEnter2D and OnTriggerEnter2D to repeat code.
+    private void HandleCollision(GameObject otherObject)
+    {
+        if (otherObject.CompareTag("EnemyWall"))
+        {
+            Destroy(gameObject);
+        }
+        if (otherObject.CompareTag("PlayerBullet"))
+        {
+            TakeDamage();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        HandleCollision(collision.gameObject);
+    }
+
+
 }
--- TASCCRCG/Assets/Scripts/Enemies/Wrench.cs
+++ TASCCRCG/Assets/Scripts/Enemies/Wrench.cs
@@ -3,101 +3,24 @@ using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class Wrench : MonoBehaviour
+public class Wrench : Enemy
 {
-    // We want access to the GameManager functions from this class.
-    [SerializeField] GameManager gameManager;
-
-    // Enemy body
-    [SerializeField] Rigidbody2D rb;
-
-    // Enemy global fields
-    private int health = 2;
-    private float damageFlashDuration = 0.1f;
-    private SpriteRenderer spriteRenderer;
-    private bool isFlashing = false;
-
     // Enemy unique fields
     [SerializeField] int baseHealth = 2;
-    [SerializeField] int scoreValue = 50;
+    [SerializeField] int baseScoreValue = 50;
     [SerializeField] float speed = 2f;
 
-
-
-    void Awake()
+    protected override void Start()
     {
-        gameManager = GameObject.FindAnyObjectByType<GameManager>();
-        spriteRenderer = GetComponent<SpriteRenderer>();
-    }
-    void Start()
-    {
+        base.Start();
         health = baseHealth + gameManager.difficulty;
+        scoreValue = baseScoreValue;
     }
 
-    void Update()
+    protected override void Update()
     {
+        base.Update();
         // Enemy movement
         transform.position += Vector3.left * Time.deltaTime * speed;
-    }
-
-    // Behaviour when the enemy is hit
-    private void TakeDamage()
-    {
-        health--;
-
-        if (health <= 0)
-        {
-            OnDestroyed();
-        }
-        else
-        {
-            // avoids flashing multiple time at the same time
-            if (!isFlashing)
-            {
-                StartCoroutine(DamageFlashCoroutine());
-            }
-        }
-    }
-
-    IEnumerator DamageFlashCoroutine()
-    {
-        isFlashing = true;
-
-        // Enemy flashes quickly when taking damage.
-        spriteRenderer.enabled = false;
-        yield return new WaitForSeconds(0.1f);
-        spriteRenderer.enabled = true;
-
-        isFlashing = false;
-
-
-    }
-
-    // Behaviour when the enemy is destroyed
-    private void OnDestroyed()
-    {
-        gameManager.playerManager.AddScore(50);
-
-        // Powerups and other effects when destroyed
-        gameManager.EnemyDown(this.gameObject);
-        GameObject.Destroy(this.gameObject);
-    }
-
-    // Handling collision to avoid OnCollisionEnter2D and OnTriggerEnter2D to repeat code.
-    private void HandleCollision(GameObject gameObject)
-    {
-        if (gameObject.CompareTag("EnemyWall"))
-        {
-            Destroy(gameObject);
-        }
-        if (gameObject.CompareTag("PlayerBullet"))
-        {
-            TakeDamage();
-        }
-    }
-
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        HandleCollision(collision.gameObject);
     }
 }
