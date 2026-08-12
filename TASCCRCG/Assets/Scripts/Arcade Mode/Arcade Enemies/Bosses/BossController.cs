68181b5cf123cc9ec7ea61b2cb6b5d08930b0a82
Vincent St-Pierre <vstpierre1@gmail.com>
2026-08-12 10:48:54 -0400

    Boss controller health and methods

--- TASCCRCG/Assets/Scripts/Arcade Mode/Arcade Enemies/Bosses/BossController.cs
+++ TASCCRCG/Assets/Scripts/Arcade Mode/Arcade Enemies/Bosses/BossController.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 
 public class BossController : MonoBehaviour
 {
+    [Header("Health")]
+    [SerializeField] private int maxHealth = 30;
+    
+    public int CurrentHealth { get; private set; }
     public bool IsDefeated { get; private set; }
 
     private void Update()
     {
+        // Test instakill
         if (Input.GetKeyDown(KeyCode.O))
         {
             DefeatBoss();
@@ -18,14 +23,36 @@ public class BossController : MonoBehaviour
     // Boss fight initialization
     public void StartFight()
     {
+        CurrentHealth = maxHealth;
         IsDefeated = false;
 
-        Debug.Log("Boss fight started!");
+        Debug.Log("Boss fight started! HP:{CurrentHealth}");
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (IsDefeated)
+        {
+            return;
+        }
+
+        CurrentHealth -= damage;
+
+        if(CurrentHealth < 0)
+        {
+            CurrentHealth = 0;
+            DefeatBoss();
+        }
     }
 
     // Boss beaten cleanup
     public void DefeatBoss()
     {
+        if (IsDefeated)
+        {
+            return;
+        }
+
         IsDefeated = true;
 
         Debug.Log("Boss Defeated");
